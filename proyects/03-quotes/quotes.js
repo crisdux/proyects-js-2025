6ca2802d58af606d239b5406cd042e616b380411
crisfer.4217 <crisfer.4217@gmail.com>
2025-03-24 20:49:44 +0000

    (feat):frontend Proyecto quotes

---
+++ proyects/03-quotes/quotes.js
@@ -0,0 +1,22 @@
+export const quotes = [
+    {
+        "quote": "No hay que ir para atrás ni para darse impulso ",
+        "author": "Lao Tsé"
+    },
+    {
+        "quote": "No hay caminos para la paz; la paz es el camino ",
+        "author": "Mahatma Gandhi"
+    },
+    {
+        "quote": "Haz el amor y no la guerra",
+        "author": "John Lennon"
+    },
+    {
+        "quote": " Para trabajar basta estar convencido de una cosa: que trabajar es menos aburrido que divertirse",
+        "author": "Charles Baudelaire"
+    },
+    {
+        "quote": "Lo peor que hacen los malos es obligarnos a dudar de los buenos",
+        "author": "Jacinto Benavente"
+    },
+];
